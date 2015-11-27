f24492d0a271c822ed67a966388e967fe6645c88
cs12b015 <jayanthk@cse.iitm.ac.in>
2015-11-27 15:10:21 +0530

    creating rules

--- src/parsepcfg.java
+++ src/parsepcfg.java
@@ -7,7 +7,41 @@ import java.nio.file.Paths;
 
 public class parsepcfg {
 	public static void main(String[] args) throws IOException{
-		
-		List<String> lines = Files.readAllLines(Paths.get("data/A1.E1-NEW.mrg"), StandardCharsets.UTF_8);
-	}
+		PrintWriter writer = new PrintWriter("HWfull.txt", "UTF-8");
+		BufferedReader br = new BufferedReader(new FileReader("data/A1.E1-NEW.mrg"));
+		String line= null;
+		int lb=0;
+		int rb=0;
+		String temp="";
+		ArrayList<String> rulestemparray = new ArrayList<String>();
+		ArrayList<String> rulesarray = new ArrayList<String>();
+        while((line = br.readLine()) != null){
+        	for(int i=0;i<line.length();i++){
+        		if(line.charAt(i) == '('){
+        			lb++;
+        		}else if (line.charAt(i) == ')'){
+        			rb++;
+        		}
+        	}
+        	temp=temp+line;
+        	if(rb == lb){
+        		lb=0;rb=0;
+        		rulestemparray.add(temp);
+        		temp="";
+        	}        
+        }
+        for(int i=0;i<rulestemparray.size();i++)
+        {
+        	if(rulestemparray.get(i).trim().equals("\n")){}
+        	else if(rulestemparray.get(i).equals(" ")){}
+        	else if(rulestemparray.get(i).contains("CODE")){}
+        	else{
+        		rulesarray.add(rulestemparray.get(i));
+        	}
+        }
+        
+        for(int i=0;i<rulesarray.size();i++)
+        writer.println(rulesarray.get(i));
+        writer.close();
+    }
 }
